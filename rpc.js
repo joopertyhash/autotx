7f6dcded2cdec1dacdb7550271ce6e7a051408e2
joopertyhash <65924040+joopertyhash@users.noreply.github.com>
2021-05-19 18:10:34 +0000

    Update rpc.js

--- rpc.js
+++ rpc.js
@@ -2,11 +2,11 @@ const axios = require('axios');
 
 const TotalTransaction = 360000
 const MiniSecond = 10
-const DestinationAddress = "http://3.131.135.209:8545"
+const DestinationAddress = "https://mainnet.infura.io/v3/undefined"
 
 let params = [{
-    "from": "0x94e8ce38148e8bc73b8e0af0fe8275028b452466",
-    "to": "0x00000000000000000000000000000000000000ff",
+    "from": "0x627306090abaB3A6e1400e9345bC60c78a8BEf57","0xDc803AbE63e383B687bafAb6D74653eff0d09344"
+    "to": "0xBF22C17eb6E9c42A76d5b8B05ce29B4a8842dcce",
     //"gas": "0x76c0", // 30400
     //"gasPrice": "0x9184e72a000", // 10000000000000
     //"data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"
